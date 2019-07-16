3857a3a2892ff6fb2d722b4b4d0d50a2db3436e6
Thalissa <thalissa@users.noreply.github.com>
2019-07-16 02:09:45 -0500

    Additional section divider, more lore.

--- src/components/toclore.vue
+++ src/components/toclore.vue
@@ -4,21 +4,40 @@
     ul.toc
       ul.section
         template(v-for="lore in loreList")
-          li.subsection
-            router-link(:to="{ path: 'lore', query: { lore: lore.name.replace(/[^a-z0-9]/gi,'') }}" tag="a" ) {{ lore.name }}
-            ul.sublist(v-for="subsection in lore.sublist")
-              li {{ subsection.name }}
+          .loreHeader
+            h3 {{ lore.name }}
+            .loreBody
+              template(v-for="section in lore.sections")
+                li.subsection
+                  router-link(:to="{ path: 'lore', query: { lore: section.name.replace(/[^a-z0-9]/gi,'') }}" tag="a" ) {{ section.name }}
+                  ul.sublist(v-for="subsection in section.sublist")
+                    li {{ subsection.name }}
 </template>
 
 <style lang="stylus" scoped>
   @import "../assets/toc.styl"
+  
+  .loreHeader
+    display flex
+    flex-direction column
+    text-align center
+    margin 15px
+    margin-left auto
+    margin-right auto
+  
+  .loreBody
+    display flex
+    flex-direction row
+    text-align left
+    flex-wrap wrap
 </style>
 
 <script>
   export default {
       data: function () {
         return {
-          loreList: ''
+          loreList: '',
+          section: ''
         }
       },
       created() {
