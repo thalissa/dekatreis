78e26077acbf2f5354c46901fee214dc27400b4e
Thalissa <thalissa@users.noreply.github.com>
2019-12-31 04:36:33 -0600

    Re-wrote how the lore page is displayed; now pulls the subsections from the lore files themselves.

--- src/components/toclore.vue
+++ src/components/toclore.vue
@@ -3,14 +3,14 @@
     h2 Table of Contents
     ul.toc
       ul.section
-        template(v-for="lore in loreList")
+        template(v-for="lore in loreData")
           .loreHeader
             h3 {{ lore.name }}
             .loreBody
-              template(v-for="section in lore.sections")
+              template(v-for="section in lore")
                 li.subsection
                   router-link(:to="{ path: 'lore', query: { lore: section.name.replace(/[^a-z0-9]/gi,'') }}" tag="a" ) {{ section.name }}
-                  ul.sublist(v-for="subsection in section.sublist")
+                  ul.sublist(v-for="subsection in section.body")
                     li {{ subsection.name }}
 </template>
 
@@ -36,13 +36,43 @@
   export default {
     data: function () {
       return {
-        loreList: '',
-        section: ''
+        loreData: { }
       }
     },
     created() {
-      var loreJSON = require("../assets/lore/lorelist.json")
-      this.loreList = loreJSON["lore"]
+      // Load JSON file with each lore sections and define it as loreList
+      let loreJSON = require("../assets/lore/lorelist.json")
+      let loreList = loreJSON["lore"]
+      
+      // Define loreListings as array for all the data manipulation we'll be doing
+      const loreListings = [ ]
+      
+      // For each section, create a new variable
+      loreList.forEach(function(lore){
+        let sections = lore.sections
+        let sectionNew = [ ]
+        
+        //Define each new section we're adding
+        sectionNew.name = lore.name
+        
+        sections.forEach(function(section){
+          // Define the contents of each new section we're adding
+          let subsection = { }
+          // These represent the attributes of the subsection
+          let sectionID = section.name.replace(/[^a-z0-9]/gi,'').toLowerCase()
+          let sectionBody = require("../assets/lore/" + sectionID + ".json")[sectionID][0].sections
+          // These insert the data to the subsection's variable
+          subsection.name = section.name
+          subsection.body = sectionBody
+          //Push subsection's variable to the new section being made
+          sectionNew.push(subsection)
+        })
+        // Push section to the loreListings variable so it displays on the page
+        loreListings.push(sectionNew)
+      })
+      
+      //Set loreData as our manipulated data
+      this.loreData = loreListings
     }
   }
 </script>
