18da1636a90fbbcae6f2106805ad98b27640f49b
Thalissa <thalissaremnant@hotmail.com>
2020-03-15 04:02:22 -0500

    Update to the map page to add links to the kingdoms
    Future plans include adding links to the Viper Sea, other important locations, and such.

--- src/components/map.vue
+++ src/components/map.vue
@@ -1,5 +1,21 @@
 <template>
   .content
+    .headerLinks
+      .link
+        router-link(:to="{ path: '/lore/Dekatreis' }" tag="a" ) Dekatreis
+      .link
+        router-link(:to="{ path: '/lore/Sarille' }" tag="a" ) Sarille
+      .link
+        router-link(:to="{ path: '/lore/Totara' }" tag="a" ) Totara
+      .link
+        router-link(:to="{ path: '/lore/AzorianEmpire' }" tag="a" ) Azorian Empire
+      .link
+        router-link(:to="{ path: '/lore/Azoranthium' }" tag="a" ) Azoranthium
+      .link
+        router-link(:to="{ path: '/lore/Akiira' }" tag="a" ) Akiira
+    
+    hr
+    
     .map
       img(:src="require('../assets/img/worldmap.jpg')")
 </template>
@@ -8,6 +24,18 @@
   .map img
     max-width 100%
     max-height 100%
+  
+  .headerLinks
+    display flex
+    flex-direction row
+    flex-grow 1
+    flex-wrap wrap
+    justify-content center
+  
+  .headerLinks .link
+    font-size 17px
+    margin-left 10px
+    margin-right 10px
 </style>
 
 <script>
